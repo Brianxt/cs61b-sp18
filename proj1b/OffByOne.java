2a6362fcf2e25f4fc8df861bf959f211f180e632
bian <xtbian1997@outlook.com>
2020-11-07 21:41:46 +0800

    first commit

--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -3,13 +3,8 @@ public class OffByOne implements CharacterComparator {
     @Override
     public boolean equalChars(char x, char y) {
         int diff = Math.abs(x - y);
-        int x1 = x;
-        int y1 = y;
-        if (97 <= x1 && x1 >= 122 && 97 <= y1 && y1 >= 122) {
-            if (diff == 1) {
-                return true;
-            }
-            return false;
+        if (diff == 1) {
+            return true;
         }
         return false;
     }
