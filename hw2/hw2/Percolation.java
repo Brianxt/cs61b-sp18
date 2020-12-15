1a838ce09bb942ac061743c3274b44a0c721da9f
bian <xtbian1997@outlook.com>
2020-12-16 00:13:46 +0800

    first commit

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,101 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private int N;
+    private boolean[] firstGrid;
+    private WeightedQuickUnionUF weightedGrid;
+    private WeightedQuickUnionUF backWash;
+    boolean percolated = false;
 
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("N is excepted to be greater than 0");
+        }
+        this.N = N;
+        firstGrid = new boolean[N * N];
+        for (int i = 0; i < N * N; i++) {
+            firstGrid[i] = false;
+        }
+        weightedGrid = new WeightedQuickUnionUF(N * N + 2);
+        backWash = new WeightedQuickUnionUF(N * N + 1);
+    }
+
+    private int xyTo1D(int row, int col) {
+        return N * row + col;
+    }
+
+    public void open(int row, int col) {
+        if (outOfBounds(row, col)) {
+            throw new IndexOutOfBoundsException("Illegal row or colon");
+        }
+        if (!firstGrid[xyTo1D(row, col)]) {
+            firstGrid[xyTo1D(row, col)] = true;
+            unionNeighborhood(row, col, row - 1, col);
+            unionNeighborhood(row, col, row + 1, col);
+            unionNeighborhood(row, col, row, col - 1);
+            unionNeighborhood(row, col, row, col + 1);
+        }
+        if (weightedGrid.connected(N * N, N * N + 1)) {
+            percolated = true;
+        }
+    }
+
+    private boolean outOfBounds(int row, int col) {
+        if (row < 0 || row >= N || col < 0 || col >= N) {
+            return true;
+        }
+        return false;
+    }
+
+    private void unionNeighborhood(int row, int col, int neighborRow, int neighborCol) {
+        if (outOfBounds(neighborRow, neighborCol)) {
+            return;
+        }
+        if ((row == 0) && (!weightedGrid.connected(xyTo1D(row, col), N * N))) {
+            weightedGrid.union(xyTo1D(row, col), N * N);
+            backWash.union(xyTo1D(row, col), N * N);
+        } else if ((row == N - 1) && (!weightedGrid.connected(xyTo1D(row, col), N * N + 1))) {
+            weightedGrid.union(xyTo1D(row, col), N * N + 1);
+        }
+        if (firstGrid[xyTo1D(neighborRow, neighborCol)] && !weightedGrid.connected(xyTo1D(row, col), xyTo1D(neighborRow, neighborCol))) {
+            weightedGrid.union(xyTo1D(row, col), xyTo1D(neighborRow, neighborCol));
+            backWash.union(xyTo1D(row, col), xyTo1D(neighborRow, neighborCol));
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        return firstGrid[xyTo1D(row, col)];
+    }
+
+    public boolean isFull(int row, int col) {
+        if (weightedGrid.connected(N * N, xyTo1D(row, col))
+                && backWash.connected(xyTo1D(row, col), N * N)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean percolates() {
+        return percolated;
+    }
+
+    /**public static void main(String[] args) {
+        Percolation p = new Percolation(4);
+        System.out.println(p.percolates());
+        p.open(1, 1);
+        p.open(1, 2);
+        System.out.println(p.isOpen(0, 1));
+        System.out.println(p.isOpen(1, 2));
+        p.open(2, 2);
+        System.out.println(p.isFull(2, 2));
+        //System.out.println(p.percolates());
+        p.open(0, 2);
+        System.out.println(p.percolates());
+        p.open(2, 3);
+        System.out.println(p.isFull(2, 3));
+        p.open(3, 3);
+        System.out.println(p.percolates());
+        p.open(3, 0);
+        System.out.println(p.isFull(3, 0));
+    }*/
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,48 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    private int N;
+    private int T;
+    private Percolation p;
+    private int[] num;
+    //private boolean flag = true;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Both N and T should be larger than 0!");
+        }
+        this.N = N;
+        this.T = T;
+        this.p = pf.make(N);
+        num = new int[T];
+        for (int i = T; i > 0; i--) {
+            while (!p.percolates()) {
+                int row = StdRandom.uniform(0, N);
+                int col = StdRandom.uniform(0, N);
+                if (!p.isOpen(row, col)) {
+                    p.open(row, col);
+                    num[i] += num[i];
+                }
+            }
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(num);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(num);
+    }
+
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
