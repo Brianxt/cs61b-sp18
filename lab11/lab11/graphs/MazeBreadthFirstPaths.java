ca37578fee9860386d16e52bc3195554014ebfa5
bian <xtbian1997@outlook.com>
2020-12-30 02:28:54 +0800

    first commit

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+//import java.util.Collection;
+//import java.util.Iterator;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +14,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    //private boolean targetFound = false;
+    private Maze maze;
+    private static final int INFINITY = Integer.MAX_VALUE;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = m.xyTo1D(sourceX, sourceY);
+        t = m.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+    private void bfs(int g) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int v = 0; v < maze.V(); v++) {
+            distTo[v] = INFINITY;
+        }
+        distTo[g] = 0;
+        marked[g] = true;
+        queue.add(g);
+        while (!queue.isEmpty()) {
+            int v = queue.remove();
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    queue.add(w);
+                }
+                announce();
+            }
+            if (queue.contains(t)) {
+                return;
+            }
+        }
     }
 
-
-    @Override
     public void solve() {
-        // bfs();
+        bfs(s);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+//import java.util.Stack;
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +12,59 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s = 0;
+    //private int b;
+    private int[] cameFrom;
 
     public MazeCycles(Maze m) {
         super(m);
     }
 
+
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cameFrom = new int[maze.V()];
+        cycle(s);
     }
 
+    Stack<Integer> vv = new Stack<>();
+
     // Helper methods go here
+    private void cycle(int v) {
+        //edgeTo[v] = v;
+        marked[v] = true;
+        if (v == 0) {
+            distTo[v] = 0;
+            vv.push(v);
+            announce();
+            cameFrom[v] = v;
+        }
+
+        for (int w : maze.adj(v)) {
+            /*if (vv.size() > 1) {
+                int a = vv.pop();
+                b = vv.peek();
+                vv.push(a);
+            }*/
+            if (marked[w] && w != cameFrom[v]) {
+                edgeTo[w] = v;
+                for (int i = 0; i < vv.size() - 1; i++) {
+                    int p = vv.pop();
+                    edgeTo[vv.peek()] = p;
+                    if (vv.peek() == w) {
+                        break;
+                    }
+                }
+            }
+            if (!marked[w]) {
+                //edgeTo[w] = v;
+                cameFrom[w] = v;
+                announce();
+                vv.push(w);
+                distTo[w] = distTo[v] + 1;
+                cycle(w);
+            }
+        }
+    }
 }
 
